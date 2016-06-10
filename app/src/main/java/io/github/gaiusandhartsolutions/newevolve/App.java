af602f1dda434feccf2804dc4a1d7608ac886848
Erik Gaius Capistrano <gaius.coffee@gmail.com>
2016-06-10 18:25:03 +0800

    Make LoginActivity work via Firebase & GoogleSignIn

--- app/src/main/java/io/github/gaiusandhartsolutions/newevolve/App.java
+++ app/src/main/java/io/github/gaiusandhartsolutions/newevolve/App.java
@@ -2,23 +2,34 @@ package io.github.gaiusandhartsolutions.newevolve;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by erik.capistrano on 6/9/2016.
  */
 public class App extends Application {
+    // Constants
+    public static final String AUTH_NAME = "AUTH_NAME";
+    public static final String AUTH_EMAIL = "AUTH_EMAIL";
+    public static final String AUTH_PHOTOURL = "AUTH_PHOTOURL";
+    // Variables
     private Context context;
     private FirebaseAnalytics fAnalytics;
+    private FirebaseAuth fAuth;
+    private SharedPreferences LDB;
 
+    // Constructor
     public App(){
         super();
     }
 
     public void setContext(Context context) {
         this.context = context;
+        this.LDB = context.getSharedPreferences(getString(R.string.app_ldb), MODE_PRIVATE);
     }
 
     public FirebaseAnalytics getLogger() {
@@ -32,10 +43,27 @@ public class App extends Application {
         return this.fAnalytics;
     }
 
+    public FirebaseAuth getAuth() {
+        if (fAuth == null) {
+            fAuth = FirebaseAuth.getInstance();
+        }
+        return this.fAuth;
+    }
+
     public void logUITouch(String UIName, String UIType) {
         Bundle bundle = new Bundle();
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, UIName);
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, UIType);
         this.getLogger().logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    }
+
+    public String getFromLDB(String key) {
+        return LDB.getString(key, "");
+    }
+
+    public void setToLDB(String key, String value) {
+        SharedPreferences.Editor editor = LDB.edit();
+        editor.putString(key, value);
+        editor.commit();
     }
 }
--- app/src/main/java/io/github/gaiusandhartsolutions/newevolve/LoginActivity.java
+++ app/src/main/java/io/github/gaiusandhartsolutions/newevolve/LoginActivity.java
@@ -1,172 +1,113 @@
 package io.github.gaiusandhartsolutions.newevolve;
 
-import android.annotation.SuppressLint;
-import android.app.Application;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class LoginActivity extends AppCompatActivity {
-    App A;
+public class LoginActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
 
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = false;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            A.logUITouch("login_button_signin", "button");
-
-            return false;
-        }
-    };
+    // Constants
+    private static final int RC_SIGN_IN = 912;
+    // Variables
+    private App A;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize App Global Object
-        this.A = (App)getApplication();
+        /* Initialize Global App Object */
+        A = (App) getApplication();
         A.setContext(getApplicationContext());
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        /* Check if already logged in */
+        if (A.getAuth().getCurrentUser() != null) {
+            goToMain();
+            return;
+        }
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        /* Initialize Google Objects */
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.app_webclientid))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        /* Configure SignInButton */
+        final SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setScopes(gso.getScopeArray());
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                toggle();
+            public void onClick(View v) {
+                A.logUITouch("sign_in_button", "button");
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.login_button_signin).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+
+                // LDB Auth
+                A.setToLDB(App.AUTH_NAME, account.getDisplayName());
+                A.setToLDB(App.AUTH_EMAIL, account.getEmail());
+                A.setToLDB(App.AUTH_PHOTOURL, account.getPhotoUrl().toString());
+
+                // Firebase Auth
+                AuthCredential credential = GoogleAuthProvider.getCredential(
+                        account.getIdToken(), null);
+                A.getAuth().signInWithCredential(credential)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this,
+                                            "Firebase Signin Failed :(", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    goToMain();
+                                }
+                            }
+                        });
+            } else {
+                Toast.makeText(this, "Google Signin Failed :(", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    public void goToMain() {
+        // TODO: Go to Main
     }
 }
