2dec8227ea07d0e5f3ea54b0923d6c49c442770e
Erik Gaius Capistrano <gaius.coffee@gmail.com>
2016-06-13 23:47:11 +0800

    Make Book NavigationMenu work.

--- app/src/main/java/io/github/gaiusandhartsolutions/newevolve/BookActivity.java
+++ app/src/main/java/io/github/gaiusandhartsolutions/newevolve/BookActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BookActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -48,20 +50,82 @@ public class BookActivity extends AppCompatActivity
 
         Intent intent = getIntent();
         this.book_contents = intent.getStringExtra(Engine.TAG_BOOK);
-        MenuItem nav_book_quickstart = (MenuItem) findViewById(R.id.nav_book_quickstart);
-        MenuItem nav_book_lkm = (MenuItem) findViewById(R.id.nav_book_lkm);
-        MenuItem nav_book_agb = (MenuItem) findViewById(R.id.nav_book_agb);
-        // MenuItem nav_book_f = (MenuItem)findViewById(R.id.nav_book_f);
+        MenuItem nav_book_quickstart = navigationView.getMenu().findItem(R.id.nav_book_quickstart);
+        MenuItem nav_book_lkm = navigationView.getMenu().findItem(R.id.nav_book_lkm);
+        MenuItem nav_book_agb = navigationView.getMenu().findItem(R.id.nav_book_agb);
+        // MenuItem nav_book_f = (MenuItem)navigationView.getMenu().findItem(R.id.nav_book_f);
+        MenuItem nav_book_c1 = navigationView.getMenu().findItem(R.id.nav_book_c1);
+        MenuItem nav_book_c2 = navigationView.getMenu().findItem(R.id.nav_book_c2);
+        MenuItem nav_book_c3 = navigationView.getMenu().findItem(R.id.nav_book_c3);
+        MenuItem nav_book_c4 = navigationView.getMenu().findItem(R.id.nav_book_c4);
+        MenuItem nav_book_c5 = navigationView.getMenu().findItem(R.id.nav_book_c5);
+        MenuItem nav_book_c6 = navigationView.getMenu().findItem(R.id.nav_book_c6);
+        MenuItem nav_book_c7 = navigationView.getMenu().findItem(R.id.nav_book_c7);
+        MenuItem nav_book_c8 = navigationView.getMenu().findItem(R.id.nav_book_c8);
+        MenuItem nav_book_c9 = navigationView.getMenu().findItem(R.id.nav_book_c9);
+        ImageView book_nav_imageView = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.book_nav_imageView);
+        TextView book_nav_title = (TextView) navigationView.getHeaderView(0).findViewById(R.id.book_nav_title);
+
         if (this.book_contents.equals(getString(R.string.book_qst))) {
             nav_book_quickstart.setChecked(true);
+            book_nav_imageView.setImageResource(R.drawable.book_quickstart);
+            book_nav_title.setText(getString(R.string.book_qst));
+            if (getString(R.string.bq_c1_title).equals("")) {
+                nav_book_c1.setVisible(false);
+            } else {
+                nav_book_c1.setTitle(getString(R.string.bq_c1_title));
+            }
+            if (getString(R.string.bq_c2_title).equals("")) {
+                nav_book_c2.setVisible(false);
+            } else {
+                nav_book_c2.setTitle(getString(R.string.bq_c2_title));
+            }
+            if (getString(R.string.bq_c3_title).equals("")) {
+                nav_book_c3.setVisible(false);
+            } else {
+                nav_book_c3.setTitle(getString(R.string.bq_c3_title));
+            }
+            if (getString(R.string.bq_c4_title).equals("")) {
+                nav_book_c4.setVisible(false);
+            } else {
+                nav_book_c4.setTitle(getString(R.string.bq_c4_title));
+            }
+            if (getString(R.string.bq_c5_title).equals("")) {
+                nav_book_c5.setVisible(false);
+            } else {
+                nav_book_c5.setTitle(getString(R.string.bq_c5_title));
+            }
+            if (getString(R.string.bq_c6_title).equals("")) {
+                nav_book_c6.setVisible(false);
+            } else {
+                nav_book_c6.setTitle(getString(R.string.bq_c6_title));
+            }
+            if (getString(R.string.bq_c7_title).equals("")) {
+                nav_book_c7.setVisible(false);
+            } else {
+                nav_book_c7.setTitle(getString(R.string.bq_c7_title));
+            }
+            if (getString(R.string.bq_c8_title).equals("")) {
+                nav_book_c8.setVisible(false);
+            } else {
+                nav_book_c8.setTitle(getString(R.string.bq_c8_title));
+            }
+            if (getString(R.string.bq_c9_title).equals("")) {
+                nav_book_c9.setVisible(false);
+            } else {
+                nav_book_c9.setTitle(getString(R.string.bq_c9_title));
+            }
         }
         if (this.book_contents.equals(getString(R.string.book_lkm))) {
             nav_book_lkm.setChecked(true);
+            book_nav_imageView.setImageResource(R.drawable.book_lkm);
+            book_nav_title.setText(getString(R.string.book_lkm));
         }
         if (this.book_contents.equals(getString(R.string.book_agb))) {
             nav_book_agb.setChecked(true);
+            book_nav_imageView.setImageResource(R.drawable.book_agb);
+            book_nav_title.setText(getString(R.string.book_agb));
         }
-
     }
 
     @Override
@@ -76,8 +140,8 @@ public class BookActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.book, menu);
+
         return true;
     }
 
